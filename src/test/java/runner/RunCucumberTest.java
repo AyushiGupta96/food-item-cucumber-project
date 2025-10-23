daebcb0d08349cecd479cd49c4a799b1ad824418
Ayushi Gupta <ayushiguota4050@gmail.com>
2025-10-24 04:45:45 +0530

    added tags

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -1,5 +1,6 @@
 package runner;
 
+import io.cucumber.core.options.Constants;
 import io.cucumber.plugin.Plugin;
 import org.junit.runners.Suite;
 
@@ -11,6 +12,9 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 @SelectPackages("test")
 @ConfigurationParameter(key= PLUGIN_PROPERTY_NAME,value="pretty")
 @ConfigurationParameter(key= GLUE_PROPERTY_NAME,value="test")
+@CofigurationParameter(Key= Constants.FILTER_TAGS_PROPERTY_NAME,value="@SmokeTest and @RegularTest")
+//@IncludeTags("SmokeTags | NightTest")
+//@IncludeTags("!NightTest") or @IncludeTags("SmokeTags & NightTest")
 public class RunCucumberTest {
 
 }
